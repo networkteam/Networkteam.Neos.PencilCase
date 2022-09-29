d2fe3e11ed49a32e263fb64f3c9b8fcb6e306f8b
Philip Schmidt <philip.schmidt@networkteam.com>
2022-09-29 11:15:21 +0200

    init basic plugin

---
+++ Networkteam.Neos.PencilCase/Classes/Controller/StandardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Networkteam\Neos\PencilCase\Controller;
+
+/*
+ * This file is part of the Networkteam.Neos.PencilCase package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class StandardController extends ActionController
+{
+
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+}
